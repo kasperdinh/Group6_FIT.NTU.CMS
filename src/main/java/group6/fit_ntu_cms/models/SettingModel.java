0b488e79e290fc70cbd52b2232491267fd4c09b3
Nguyễn  Đức Anh <anh.nd.64cntt@ntu.edu.vn>
2025-06-07 12:15:11 +0700

    footer

--- src/main/java/group6/fit_ntu_cms/models/SettingModel.java
+++ src/main/java/group6/fit_ntu_cms/models/SettingModel.java
@@ -3,6 +3,7 @@ package group6.fit_ntu_cms.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.w3c.dom.Text;
 
 @Setter
 @Getter
@@ -14,5 +15,13 @@ public class SettingModel {
     private Long id ;
     private String siteName;
     private String logoUrl;
-
+    private String footerName;
+    @Column(columnDefinition = "TEXT")
+    private String footerText;
+    private String footerFacebookUrl;
+    private String footerTwitterUrl;
+    private String footerYoutubeUrl;
+    private String footerInstagramUrl;
+    private String footerPhone;
+    private String footerEmail;
 }
